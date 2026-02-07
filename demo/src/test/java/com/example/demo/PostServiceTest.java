3d6d6ea43cf16037f82799145dd1fd210f6abdaa
kai Nishio <kairi240@icloud.com>
2026-02-07 19:30:54 +0900

    CI/CDが導入できてるかテスト

---
+++ demo/src/test/java/com/example/demo/PostServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PostServiceTest {
+    @Test
+    void simpleMathTest() {
+        int result = 1 + 1;
+        assertEquals(2, result, "1+1は2であるべき");
+    }
+}
